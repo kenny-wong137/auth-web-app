e4c7a6501ee8f8b1b7fddac836945e07ebea685f
Kenny Wong <wky123@hotmail.co.uk>
2020-05-04 21:58:08 +0100

    ssl version

--- local-install.sh
+++ local-install.sh
@@ -10,10 +10,21 @@ curl -L https://github.com/docker/compose/releases/download/1.25.5/run.sh \
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-# Create folders to store postgres files and nginx config
+# Create folders to store postgres files, nginx config and ssl certificate
+[ -d $HOME/dbfiles ] && rm -rf $HOME/dbfiles
+[ -d $HOME/nginx-conf ] && rm -rf $HOME/nginx-conf
+[ -d $HOME/certs ] && rm -rf $HOME/certs
 mkdir -p $HOME/dbfiles
 mkdir -p $HOME/nginx-conf
-cp nginx-app.conf $HOME/nginx-conf
+mkdir -p $HOME/certs
+if [ -f ssl-certificate.crt ] && [ -f ssl-certificate.key ]
+then
+  cp nginx-app-ssl.conf $HOME/nginx-conf
+  cp ssl-certificate.crt $HOME/certs
+  cp ssl-certificate.key $HOME/certs
+else
+  cp nginx-app.conf $HOME/nginx-conf
+fi
 
 # Start docker containers
 docker-compose up -d
--- readme.md
+++ readme.md
@@ -4,5 +4,8 @@ The app allows users to post their own messages and to read messages posted by o
 Local deployment (Amazon Linux / Centos):
 Run as root `bash local-install.sh`, then open `http://localhost/` in Chrome.
 
-If deploying something like this for real, make sure to use HTTPS,
-and remember to swap POSTGRES_PASSWORD and JWT_SECRET for real secrets.
+If deploying something like this for real,
+remember to swap POSTGRES_PASSWORD and JWT_SECRET for real secrets.
+Also make sure to use https. If you save your certificate and key at the
+top level of this repo folder as `ssl-certificate.crt` and `ssl-certificate.key`,
+then the install script will set up the https connection for you.
